df51f1055672a2fa712220a0b6071c1c1b908302
Rene <renatatwins18@gmail.com>
2023-05-21 00:49:10 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,28 +1,22 @@
 const CORS_ALLOWED = [
   'http://localhost:3000',
-  'https://heyRene.nomoredomains.monster',
-  'https://api.heyRene.nomoredomains.monster',
+  'https://heyrene.nomoredomains.monster',
+  'https://api.heyrene.nomoredomains.monster',
 ];
 
 const handleCors = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-  // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  // проверяем, что источник запроса есть среди разрешённых
   if (CORS_ALLOWED.includes(origin)) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', origin);
   }
-
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', '*');
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
   return next();
