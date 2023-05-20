76218a259f6e1b9d53de3bb3fef3ba1acbdf8f45
Rene <renatatwins18@gmail.com>
2023-05-20 20:06:31 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,21 +5,18 @@ const CORS_ALLOWED = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const cors = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (CORS_ALLOWED.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
+  res.header('Access-Control-Allow-Origin', '*');
 
-  return next();
+  next();
 };
 
 module.exports = { cors };
