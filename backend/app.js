5df8b8c3e82969ae75f21bd3048f5f7cb73feeb7
Rene <renatatwins18@gmail.com>
2023-05-20 21:59:00 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
 const limiter = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { cors } = require('./middlewares/cors');
+const { handleCors } = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
-app.use(cors());
+app.use(cors(handleCors));
 app.use(limiter);
 app.use(helmet());
 app.use(express.json());
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,24 @@
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const CORS_ALLOWED = [
+  'http://localhost:3001',
+  'https://heyRene.nomoredomains.monster',
+  'https://api.heyRene.nomoredomains.monster',
+];
 
-const cors = (req, res, next) => {
+const handleCors = (req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  if (CORS_ALLOWED.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  res.header('Access-Control-Allow-Origin', '*');
 
   return next();
 };
 
-module.exports = { cors };
+module.exports = { handleCors };
