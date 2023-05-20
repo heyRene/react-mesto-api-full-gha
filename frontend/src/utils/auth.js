5ca97cd12419114321efcb06b599a932c945ddd4
Rene <renatatwins18@gmail.com>
2023-05-20 15:53:07 +0300

    frontend added and urls changed

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,40 @@
+export const BASE_URL = "https://api.heyRene.nomoredomains.monster";
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+export function register({ email, password }) {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse)
+}
+export function authorize(email, password) {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+}
+export function checkToken(jwt) {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+    .then(checkResponse);
+}
