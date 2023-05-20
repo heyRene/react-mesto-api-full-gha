353d6125275881a368b4d59beb9cb71451da2aaa
Rene <renatatwins18@gmail.com>
2023-05-21 01:45:54 +0300

    token

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -88,7 +88,7 @@ function App() {
         if (data.token) {
           setEmail(email);
           handleLoggedIn();
-          localStorage.setItem("token", data.token);
+          localStorage.setItem("jwt", data.token);
           navigate("/", { replace: true });
           console.log(isLoggedIn);
         }
