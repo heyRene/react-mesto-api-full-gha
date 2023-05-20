5d14f4a8d686c07d1546f8dc2875d5ae44f6ae23
Rene <renatatwins18@gmail.com>
2023-05-20 22:24:46 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
 const limiter = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const { handleCors } = require('./middlewares/cors');
+const { handleCors } = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
-app.use(cors());
+app.use(cors(handleCors()));
 app.use(limiter);
 app.use(helmet());
 app.use(express.json());
