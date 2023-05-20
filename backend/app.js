5e33e08be47b0ce8be2d49b9da79210af00c38c3
Rene <renatatwins18@gmail.com>
2023-05-20 22:36:13 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
-
+app.options('*', cors());
 app.use(cors({
   credentials: true,
   origin: CORS_ALLOWED,
