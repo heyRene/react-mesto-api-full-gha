158fa6376c52d55c1ee52e40d766540070147cc3
Rene <renatatwins18@gmail.com>
2023-05-21 18:47:21 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,6 +2,8 @@ const CORS_ALLOWED = [
   'http://localhost:3000',
   'https://heyrene.nomoredomains.monster',
   'https://api.heyrene.nomoredomains.monster',
+  'http://api.heyrene.nomoredomains.monster',
+  'http://heyrene.nomoredomains.monster',
 ];
 
 const handleCors = (req, res, next) => {
@@ -9,13 +11,14 @@ const handleCors = (req, res, next) => {
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (CORS_ALLOWED.includes(origin)) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', origin);
   }
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
