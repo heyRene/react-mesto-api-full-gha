329d052057da9e52a1a921562520e46144c48379
Rene <renatatwins18@gmail.com>
2023-05-21 11:49:17 +0300

    crash test

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -2,6 +2,11 @@ const signinRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
 
+signinRouter.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 signinRouter.post(
   '/',
   celebrate({
