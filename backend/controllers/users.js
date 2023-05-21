782eff7fb3c0574c8c974ef08a0265c324d5e8be
Rene <renatatwins18@gmail.com>
2023-05-21 14:28:19 +0300

    users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -51,26 +51,21 @@ const login = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  } = req.body;
-
-  bcrypt.hash(password, 10)
+  bcrypt.hash(req.body.password, 10)
     .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      email: req.body.email,
       password: hash,
     }))
     .then((newUser) => {
-      const data = newUser.toObject();
-      delete data.password;
-      res.send(data);
+      res.status(201).send({
+        email: newUser.email,
+        name: newUser.name,
+        about: newUser.about,
+        avatar: newUser.avatar,
+      });
     })
     .catch((err) => {
       if (err.code === 11000) {
