46aebcf4eac073df20964b54f057cfa22f5067e2
Rene <renatatwins18@gmail.com>
2023-06-03 23:43:27 +0300

    backend

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -9,10 +9,6 @@ signupRouter.post(
       email: Joi.string().required().email(),
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(
-        /^(http|https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im,
-      ),
     }),
   }),
   createUser,
